ae0f43c6fa8f8fedfcc73d1117c2bb052aaf3e3a
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-07 23:59:10 +0200

    Add Value migration and model

---
+++ app/database/migrations/2013_05_07_215025_create_values_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateValuesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('values', function($table) {
+            $table->increments('id');
+            $table->integer('stock_id')->unsigned();
+            $table->timestamp('date');
+            $table->decimal('open', 5, 2);
+            $table->decimal('close', 5, 2);
+            $table->decimal('high', 5, 2);
+            $table->decimal('low', 5, 2);
+            $table->integer('volume')->unsigned();
+            $table->foreign('stock_id')->references('id')->on('stocks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('values');
+    }
+
+}
