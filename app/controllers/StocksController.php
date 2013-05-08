8dc82a8cb1cba47c4595bc913344bba463c7345b
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-08 14:36:06 +0200

    Better checking if is the title, instead of if it's not the title

--- app/controllers/StocksController.php
+++ app/controllers/StocksController.php
@@ -9,20 +9,19 @@ class StocksController extends BaseController {
 		$l = 0;
         while (($data = fgetcsv($handle, 5000, ",")) !== FALSE) {
             $l ++;
-            if ($l > 1)
-            {
-            	$datas[$data[0]]['stock_id'] = (int) $id;
-                $datas[$data[0]]['date'] = $data[0];
-                $datas[$data[0]]['open'] = Value::toInt($data[1]);
-                $datas[$data[0]]['close'] = Value::toInt($data[4]);
-                $datas[$data[0]]['high'] = Value::toInt($data[2]);
-                $datas[$data[0]]['low'] = Value::toInt($data[3]);
-                $datas[$data[0]]['volume'] = (int) $data[5];
-                $delta = (Value::toInt($data[4]) - Value::toInt($data[1])) / Value::toInt($data[1]);
-                $datas[$data[0]]['delta'] = (int) ($delta * 10000);
-                $absdelta = (Value::toInt($data[2]) - Value::toInt($data[3])) / Value::toInt($data[2]);
-                $datas[$data[0]]['absdelta'] = (int) ($absdelta * 10000);
-            }
+            if ($l == 1)
+                continue;
+       	    $datas[$data[0]]['stock_id'] = (int) $id;
+            $datas[$data[0]]['date'] = $data[0];
+            $datas[$data[0]]['open'] = Value::toInt($data[1]);
+            $datas[$data[0]]['close'] = Value::toInt($data[4]);
+            $datas[$data[0]]['high'] = Value::toInt($data[2]);
+            $datas[$data[0]]['low'] = Value::toInt($data[3]);
+            $datas[$data[0]]['volume'] = (int) $data[5];
+            $delta = (Value::toInt($data[4]) - Value::toInt($data[1])) / Value::toInt($data[1]);
+            $datas[$data[0]]['delta'] = (int) ($delta * 10000);
+            $absdelta = (Value::toInt($data[2]) - Value::toInt($data[3])) / Value::toInt($data[2]);
+            $datas[$data[0]]['absdelta'] = (int) ($absdelta * 10000);
         }
 
         $present = Value::where('stock_id', $id)->lists('id', 'date');
