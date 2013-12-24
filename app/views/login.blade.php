f83764e70f9e20c3e4f859b543d64d483cb7c7f6
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-24 23:26:16 +0100

    Nicer login form

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,12 +1,12 @@
 @extends('layout')
 
 @section('content')
-    {{ Form::open(array('url'=>'users/signin', 'class'=>'form-signin')) }}
-       <h2 class="form-signin-heading">Please Login</h2>
+    {{ Form::open(array('url'=>'users/signin', 'class'=>'form')) }}
+       <h2 class="form-signin-heading">Login</h2>
      
-       {{ Form::text('email', null, array('class'=>'input-block-level', 'placeholder'=>'Email Address')) }}
-       {{ Form::password('password', array('class'=>'input-block-level', 'placeholder'=>'Password')) }}
-     
+       {{ Form::text('email', null, array('class'=>'form-control', 'placeholder'=>'Email Address')) }}
+       {{ Form::password('password', array('class'=>'form-control', 'placeholder'=>'Password')) }} 
        {{ Form::submit('Login', array('class'=>'btn btn-large btn-primary btn-block'))}}
+
     {{ Form::close() }}
 @stop
