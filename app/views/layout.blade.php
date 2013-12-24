9d03a4ddcabea821c343f347d5b9af3ea764962a
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-24 22:56:06 +0100

    Add header

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -11,6 +11,24 @@
     <![endif]-->
   </head>
   <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Finance</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Peaks</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
     <div class="container">
       @if(Session::has('message'))
         <p class="alert">{{ Session::get('message') }}</p>
