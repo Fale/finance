7d94526649d15ba1cd4cb022742a12cbe38aaca5
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-24 23:07:41 +0100

    Make the navbar better

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -14,17 +14,18 @@
     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
           <a class="navbar-brand" href="#">Finance</a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Peaks</a></li>
+            <li class="active"><a href="/stocks/peaks">Peaks</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            @if(!Auth::check())
+               <li>{{ HTML::link('users/login', 'Login') }}</li>   
+            @else
+               <li>{{ HTML::link('users/logout', 'logout') }}</li>
+            @endif
           </ul>
         </div><!--/.nav-collapse -->
       </div>
