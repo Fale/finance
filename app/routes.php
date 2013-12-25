9056a8cf89873fda793cc2e7eabe2e01bc402e54
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-25 10:40:41 +0100

    Forgot ;

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('/', function() {return View::make('home');})
+Route::get('/', function() {return View::make('home');});
 Route::controller('stocks', 'StocksController');
 Route::controller('users', 'UsersController');
