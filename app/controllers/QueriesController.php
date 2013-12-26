af2d27b69f8b1ccb6538cc110878c106c2d308b1
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-26 17:59:25 +0100

    Improve code

--- app/controllers/QueriesController.php
+++ app/controllers/QueriesController.php
@@ -16,16 +16,25 @@ class QueriesController extends BaseController {
 
     public function postNew()
     {
-        return Input::all();
+        $filters = Input::get('filters');
 
-        /*$query = DB::table('node');
+        $this->layout->content = var_dump(Input::get('filters'));
+    }
 
+    public function getQuery()
+    {
+        $qid = Input::get('query');
+
+        $query = DB::table('node');
+
+        foreach( $filters as $filter){
         if ($published == true)
             $query->where('published', '=', 1);
 
         if (isset($year))
             $query->where('year', '>', $year);
+        }
+        $result = $query->get();
 
-        $result = $query->get();*/
     }
 }
