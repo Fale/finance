7ea5357b735c7898c8a3e286eb281723ef3539f4
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-26 17:41:29 +0100

    Now the queries.new UI works as expected

--- app/views/queries/new.blade.php
+++ app/views/queries/new.blade.php
@@ -12,9 +12,9 @@
 
 @section('js')
   <script>
-    var i = 0;
+    var i = 1;
     $( "button#add" ).click(function() {
-        $("input#submit").before('<div class="form-vertical"><div class="form-group">{{ HTML::Decode(Form::select('filters[\' + i + \'].type', array( '' => '', 'HV' => 'High value', 'LV' => 'Low value', 'D' => 'Delta'), '', array('class' => 'form-control type', 'onChange' => 'loadInputs(this, i)')))}}</div></div>');
+        $("input#submit").before('<div class="form-vertical"><div class="form-group">{{ HTML::Decode(Form::select('filters[\' + i + \'][type]', array( '' => '', 'HV' => 'High value', 'LV' => 'Low value', 'D' => 'Delta'), '', array('class' => 'form-control type', 'onChange' => 'loadInputs(this, \' + i + \')')))}}</div></div>');
         i++;
     });
 
@@ -22,16 +22,16 @@
       console.log('changed!')
       if($(item).val() == 'HV'){
 {{--        $(item).parent().parent().append('<div class="form-group">{{ Form::select(HTML::Decode('filters.["\'index\']_sign'), array('>', '=', '<', '=>', '<='), '', array('class' => 'form-control')) }}</div>'); The following is the crappy workaround for this --}}
-        $(item).parent().parent().append('<div class="form-group"><select class="form-control" name="filters[' + index + '].sign"><option value="0">&gt;</option><option value="1">=</option><option value="2">&lt;</option><option value="3">=&gt;</option><option value="4">&lt;=</option></select></div>');
-        $(item).parent().parent().append('<div class="form-group">{{ HTML::Decode(Form::text('filters[\' + index + \'].value', '', array('class' => 'form-control'))) }}</div>');
+        $(item).parent().parent().append('<div class="form-group"><select class="form-control" name="filters[' + index + '][sign]"><option value="0">&gt;</option><option value="1">=</option><option value="2">&lt;</option><option value="3">=&gt;</option><option value="4">&lt;=</option></select></div>');
+        $(item).parent().parent().append('<div class="form-group">{{ HTML::Decode(Form::text('filters[\' + index + \'][value]', '', array('class' => 'form-control'))) }}</div>');
       }
       if($(item).val() == 'LV'){
-        $(item).parent().parent().append('<div class="form-group"><select class="form-control" name="filters[' + index + '].sign"><option value="0">&gt;</option><option value="1">=</option><option value="2">&lt;</option><option value="3">=&gt;</option><option value="4">&lt;=</option></select></div>');
-        $(item).parent().parent().append('<div class="form-group">{{ HTML::Decode(Form::text('filters[\' + index + \'].value', '', array('class' => 'form-control'))) }}</div>');
+        $(item).parent().parent().append('<div class="form-group"><select class="form-control" name="filters[' + index + '][sign]"><option value="0">&gt;</option><option value="1">=</option><option value="2">&lt;</option><option value="3">=&gt;</option><option value="4">&lt;=</option></select></div>');
+        $(item).parent().parent().append('<div class="form-group">{{ HTML::Decode(Form::text('filters[\' + index + \'][value]', '', array('class' => 'form-control'))) }}</div>');
       }
       if($(item).val() == 'D'){
-        $(item).parent().parent().append('<div class="form-group"><select class="form-control" name="filters[' + index + '].sign"><option value="0">&gt;</option><option value="1">=</option><option value="2">&lt;</option><option value="3">=&gt;</option><option value="4">&lt;=</option></select></div>');
-        $(item).parent().parent().append('<div class="form-group">{{ HTML::Decode(Form::text('filters[\' + index + \'].value', '', array('class' => 'form-control'))) }}</div>');
+        $(item).parent().parent().append('<div class="form-group"><select class="form-control" name="filters[' + index + '][sign]"><option value="0">&gt;</option><option value="1">=</option><option value="2">&lt;</option><option value="3">=&gt;</option><option value="4">&lt;=</option></select></div>');
+        $(item).parent().parent().append('<div class="form-group">{{ HTML::Decode(Form::text('filters[\' + index + \'][value]', '', array('class' => 'form-control'))) }}</div>');
       }
     };
   </script>
