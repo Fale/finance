a70e5088921a9f34473551fa7d474cd7547063f5
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-26 17:30:19 +0100

    Add eample

--- app/controllers/QueriesController.php
+++ app/controllers/QueriesController.php
@@ -2,20 +2,30 @@
 
 class QueriesController extends BaseController {
 
-	protected $layout = 'dummy';
+    protected $layout = 'dummy';
 
-	public function getIndex()
-	{
-		$this->layout->content = View::make('queries/index');
-	}
+    public function getIndex()
+    {
+        $this->layout->content = View::make('queries/index');
+    }
 
-	public function getNew()
-	{
-		$this->layout->content = View::make('queries/new');
-	}
+    public function getNew()
+    {
+        $this->layout->content = View::make('queries/new');
+    }
 
-	public function postNew()
-	{
-		return Input::all();
-	}
+    public function postNew()
+    {
+        return Input::all();
+
+        /*$query = DB::table('node');
+
+        if ($published == true)
+            $query->where('published', '=', 1);
+
+        if (isset($year))
+            $query->where('year', '>', $year);
+
+        $result = $query->get();*/
+    }
 }
