a1f1d9c03d26d4859902571d1dc1927e127a05f4
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-26 10:28:43 +0100

    Fix auth permissions for homepage

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('/', function() {return View::make('home');});
+Route::get('/', array('before' => 'auth', function() {return View::make('home');}));
 Route::controller('stocks', 'StocksController');
 Route::controller('users', 'UsersController');
 Route::controller('queries', 'QueriesController');
