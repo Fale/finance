d69c934bc1b9dc22ed838bb7e8f3cff50ebc5029
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-02-01 17:53:03 +0100

    Add favicon

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
     @yield('css', '')
     <link href="/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="/favicon.ico" />
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
